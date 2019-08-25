79e73e0f1b61a147c8bc1d73639a669747415b22
haniokasai@Desktop <htek@haniokasai.com>
2019-08-26 00:21:22 +0900

    getFileの修正 TODO:コンフィグの読み込み

--- getFiles.sh
+++ getFiles.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -O resources/bds.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.12.0.28.zip
+wget -O resources/bds.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.12.0.28.zip
