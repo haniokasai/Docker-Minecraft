634202ed1a82db2dc46dd32122532d0c41468107
haniokasai@Desktop <htek@haniokasai.com>
2019-08-26 16:03:36 +0900

    pharの正当性チェック

---
+++ resources/pharvalid.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hani
+ * Date: 2018/11/15
+ * Time: 1:50
+ */
+// php pharvalid.php phar名　でレスポンス。
+for(;;) {
+
+    $filename = $argv[1];
+    if (!isset($filename)) {
+        echo "Eneedarg";
+        break;
+    }
+
+    if (!file_exists($filename)) {
+        echo "Efilenotfound";
+        break;
+    }
+
+    if (!Phar::isValidPharFilename($filename)){
+        echo "EisValidPharFilename:false";
+        break;
+    }
+
+    try {
+        $p = new Phar($filename, 0, "mochikomi.phar");
+        $hash = $p->getSignature()["hash"];
+        echo "##hash##".$p->getSignature()["hash"]."##hash##";
+        break;
+    }catch (Exception $e){
+        echo "Epharstructureerror";
+        break;
+    }
+
+    break;
+
+}
+exit();
