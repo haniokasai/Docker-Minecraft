d1682a764bc396b24f5254c972a4a7308f7f4176
haniokasai@Desktop <htek@haniokasai.com>
2019-08-30 01:16:07 +0900

    resourcesの重複

--- resources/run-BE-BDS.sh
+++ resources/run-BE-BDS.sh
@@ -10,8 +10,8 @@ cd ${WDIR}/bds/
 unzip bds.zip
 rm -rf bds.zip
 rm -rf /minecraft/bin/bedrock_server
-mv ${WDIR}/resources/bds/bedrock_server /minecraft/bin/bedrock_server
-rsync ${WDIR}/resources/bds/ /minecraft/server/ -av --delete --exclude worlds --exclude server.properties --exclude ops.json --exclude whitelist.json --exclude permissions.json
+mv ${WDIR}/bds/bedrock_server /minecraft/bin/bedrock_server
+rsync ${WDIR}/bds/ /minecraft/server/ -av --delete --exclude worlds --exclude server.properties --exclude ops.json --exclude whitelist.json --exclude permissions.json
 
 sh /minecraft/resources/setPerm.sh
 /minecraft/bin/bedrock_server
