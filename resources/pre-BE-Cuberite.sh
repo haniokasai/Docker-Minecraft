c86fe06726cddc2c8af0a887c9ee148b40b50184
haniokasai@Desktop <htek@haniokasai.com>
2019-08-30 01:06:05 +0900

    cubeerite

--- resources/pre-BE-Cuberite.sh
+++ resources/pre-BE-Cuberite.sh
@@ -2,6 +2,7 @@
 echo "pre process...." >&1
 
 WDIR=/minecraft/resources
+cd ${WDIR}
 tar zxvf ${WDIR}/cuberite*.tar.gz
 mv ${WDIR}/cuberite_binfile /minecraft/bin/cuberite_binfile
 mv ${WDIR}/cuberite_src/* /minecraft/server/
