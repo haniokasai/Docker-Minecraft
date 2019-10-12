ededb4fed9e2a8c422f58192d0425428f02a9cd5
haniokasai@Desktop <htek@haniokasai.com>
2019-10-12 22:53:53 +0900

    /crashdump.phpのスクレイピング

--- resources/crashdump.php
+++ resources/crashdump.php
@@ -1,10 +1,10 @@
 <?php
 //https://stackoverflow.com/questions/11597421/how-to-get-the-newest-file-in-a-directory-in-php
 @$files = scandir('/minecraft/server/crashdumps/', SCANDIR_SORT_DESCENDING);
-if(@$newest_file = $files[0]){
+if(@$newest_file = "/minecraft/server/crashdumps/".$files[0]){
 	//https://memorandum-plus.com/2018/06/07/php-文字列中から特定の部分を切り出す（substr、strlen、strpos/
 	//対象の文字列
-	@$target_text = fgets(fopen($newest_file, 'r'));
+	@$target_text = fread(fopen($newest_file, 'r'),filesize($newest_file));
 	//開始位置
 	@$start_position = strpos($target_text, "===BEGIN CRASH DUMP===") + strlen("===BEGIN CRASH DUMP===");
 	//切り出す部分の長さ
---
+++ resources/crashdump.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/minecraft/bin/bin/php7/bin/php /minecraft/resources/crashdump.php
