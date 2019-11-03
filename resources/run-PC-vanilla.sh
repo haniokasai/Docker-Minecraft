a8d23a22cd6754599935b37740f7c4a08d0606e1
haniokasai@Desktop <htek@haniokasai.com>
2019-11-04 00:08:36 +0900

    http://extrose.sblo.jp/article/185953036.html joptsimple.UnrecognizedOptionException: X is not a recognized option

--- resources/run-PC-vanilla.sh
+++ resources/run-PC-vanilla.sh
@@ -8,6 +8,6 @@ cd /minecraft/server
 echo "if you do not agree eura, please stop now...." >&1
 echo "eula=true" > eula.txt
 i=(`awk '/^Mem/ {printf("%u", $7);}' <(free -m)`)
-java -jar /minecraft/bin/mcpc.jar -Xmx$((i/10*7))m nogui
+java -Xmx$((i/10*7))m -jar /minecraft/bin/mcpc.jar  nogui
 echo "run vanilla....done" >&1
 
