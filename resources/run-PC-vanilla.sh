688f9a11037ac26e4920d2c41bccf16c434d2301
haniokasai@Desktop <htek@haniokasai.com>
2019-11-03 23:53:03 +0900

    改行コードのせいか？

--- resources/run-PC-vanilla.sh
+++ resources/run-PC-vanilla.sh
@@ -7,7 +7,7 @@ sh /minecraft/resources/blockUDP.sh
 cd /minecraft/server
 echo "if you do not agree eura, please stop now...." >&1
 echo "eula=true" > eula.txt
-i=`awk '/^Mem/ {printf("%u", $7);}'<(free -m)`
+ i=`awk '/^Mem/ {printf("%u", $7);}'<(free -m)`
 java -jar /minecraft/bin/mcpc.jar -Xmx$((i/10*7))m nogui
 echo "run vanilla....done" >&1
 
