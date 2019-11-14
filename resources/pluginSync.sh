bc4901b23b713e0221235085f19deb239871e251
haniokasai@Desktop <htek@haniokasai.com>
2019-11-14 18:04:02 +0900

    プラグインも設置

--- resources/pluginSync.sh
+++ resources/pluginSync.sh
@@ -2,6 +2,6 @@
 echo "run rsync...." >&1
 
 mkdir -p /minecraft/defaultplugins/
-tar zxvf /minecraft/resources/defaultplugins*.tar.gz -C /minecraft/defaultplugins/
+tar zxvf /minecraft/resources/defaultplugins.tar.gz -C /minecraft/defaultplugins/
 rsync -aq  --include="*/" --include="*.phar" --exclude="*" /minecraft/defaultplugins/ /minecraft/server/plugins --delete
 echo "run rsync....done" >&1
