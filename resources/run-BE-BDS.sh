3efaf615e142c89f4eff9badf2c5c7fe4a2fc4f8
haniokasai@Desktop <htek@haniokasai.com>
2019-11-14 19:01:36 +0900

    ハッシュを調べない

--- resources/run-BE-BDS.sh
+++ resources/run-BE-BDS.sh
@@ -7,34 +7,21 @@ mkdir -p ${WDIR}/bds
 
 if [ -e ${WDIR}/bds.zip ]; then
 	echo "${WDIR}/bds.zip is exist"  >&1
-	if [  ! -e "/minecraft/bin/BDShash" ]; then
-		touch /minecraft/bin/BDShash
-	fi
-	NEWHASH=`md5sum ${WDIR}/bds.zip`
-	declare file_content=$( cat /minecraft/bin/BDShash )
-
-	if [ "${file_content}" =~ "${NEWHASH}" ]; then
-		echo "HASH is SAME"  >&1
-	else
-		echo "HASH is DIFFERENT"  >&1
-		md5sum ${WDIR}/bds.zip > /minecraft/bin/BDShash
-		rm -rf ${WDIR}/bds/*
-		cd ${WDIR}
-		mv ${WDIR}/bds.zip ${WDIR}/bds/bds.zip
-		cd ${WDIR}/bds/
-		unzip -qq bds.zip
-		rm -rf bds.zip
-		rm -rf /minecraft/bin/bedrock_server
-		rm -rf /minecraft/bin/bedrock_server
-		rm -rf /minecraft/bin/libCrypto.so
-		mv ${WDIR}/bds/bedrock_server /minecraft/bin/bedrock_server
-		mv ${WDIR}/bds/libCrypto.so /usr/local/lib/libCrypto.so
-		rsync ${WDIR}/bds/ /minecraft/server/ -aq --delete --exclude worlds --exclude server.properties --exclude ops.json --exclude whitelist.json --exclude permissions.json --exclude backup
-	fi
+	md5sum ${WDIR}/bds.zip > /minecraft/bin/BDShash
+	rm -rf ${WDIR}/bds/*
+	cd ${WDIR}
+	mv ${WDIR}/bds.zip ${WDIR}/bds/bds.zip
+	cd ${WDIR}/bds/
+	unzip -qq bds.zip
+	rm -rf bds.zip
+	rm -rf /minecraft/bin/bedrock_server
+	rm -rf /minecraft/bin/libCrypto.so
+	mv ${WDIR}/bds/bedrock_server /minecraft/bin/bedrock_server
+	mv ${WDIR}/bds/libCrypto.so /usr/local/lib/libCrypto.so
+	rsync ${WDIR}/bds/ /minecraft/server/ -aq --delete --exclude worlds --exclude server.properties --exclude ops.json --exclude whitelist.json --exclude permissions.json --exclude backup
 else
 	echo "${WDIR}/bds.zip is NOT exist"  >&1
 fi
-
 
 sh /minecraft/resources/setPerm.sh
 sh /minecraft/resources/blockTCP.sh
