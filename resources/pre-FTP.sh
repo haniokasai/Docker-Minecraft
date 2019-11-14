59358368bcee4fe2ac6df17147c9396ba9f9063d
haniokasai@Desktop <htek@haniokasai.com>
2019-11-14 18:39:20 +0900

    実行フォルダがおかしい

--- resources/pre-FTP.sh
+++ resources/pre-FTP.sh
@@ -22,7 +22,7 @@ groupadd ftpgroup
 ftp_login=${SRVID}
 ftp_pass=${PASSWD}
 CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $ftp_pass)
-useradd --shell /bin/sh -d /minecraft/server --password $CRYPTED_PASSWORD $ftp_login
+useradd --shell /bin/bash -d /minecraft/server --password $CRYPTED_PASSWORD $ftp_login
 usermod $ftp_login -G ftpgroup
 
 unset ftp_pass
--- resources/run-PC-Cuberite.sh
+++ resources/run-PC-Cuberite.sh
@@ -4,6 +4,6 @@ echo "run cuberite...." >&1
 sh /minecraft/resources/setPerm.sh
 sh /minecraft/resources/blockUDP.sh
 
-su -l ${SRVID} -c "cd /minecraft/server & /minecraft/bin/cuberite_binfile"
+su -l ${SRVID} -c "cd /minecraft/server ; /minecraft/bin/cuberite_binfile"
 echo "run cuberite....done" >&1
 
