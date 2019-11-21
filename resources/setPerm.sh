7adb31b4fc9e7aaffc1e4e14458a3af8b7cf5c18
haniokasai@Desktop <htek@haniokasai.com>
2019-11-22 01:52:42 +0900

    backupfolderはもうない

--- resources/setPerm.sh
+++ resources/setPerm.sh
@@ -5,8 +5,6 @@ chgrp ftpgroup /minecraft/server -R
 chown root:root /minecraft/server
 chmod 2755 /minecraft/server 
 chmod 2777 /minecraft/server/* -R
-chmod 2777 /minecraft/server/backup -R
-
 chmod 2111 /minecraft/bin -R
 
 echo "chmod....done" >&1
