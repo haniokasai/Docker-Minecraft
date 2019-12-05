40a1cd157358bf00bfdbfe966ef71095f45b5f91
haniokasai@Desktop <htek@haniokasai.com>
2019-12-05 23:49:45 +0900

    chmod resourceにかぶらんように

--- resources/setPerm.sh
+++ resources/setPerm.sh
@@ -3,12 +3,16 @@ echo "chmod...." >&1
 
 #For sshd  bad ownership or modes for chroot directory "/minecraft/server"
 chown root  /minecraft/
-chmod 2755 -R  /minecraft/
+chmod 2755  /minecraft/
 ##
 
 chgrp ftpgroup /minecraft/server -R
 chown ${SRVID}:ftpgroup /minecraft/server/ -R
 
 chmod 2777 /minecraft/server/ -R
+
+chmod 2755 /minecraft/bin -R
+chmod 2755 /minecraft/restore -R
+chmod 2755 /minecraft/defaultplugins -R
 
 echo "chmod....done" >&1
