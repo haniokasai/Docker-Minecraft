8c472da00e58d14529e208dbafe3c2798e47a18c
haniokasai@Desktop <htek@haniokasai.com>
2019-12-05 23:47:46 +0900

    set perm for java

--- resources/setPerm.sh
+++ resources/setPerm.sh
@@ -3,16 +3,12 @@ echo "chmod...." >&1
 
 #For sshd  bad ownership or modes for chroot directory "/minecraft/server"
 chown root  /minecraft/
-chmod 2755  /minecraft/
+chmod 2755 -R  /minecraft/
 ##
 
 chgrp ftpgroup /minecraft/server -R
 chown ${SRVID}:ftpgroup /minecraft/server/ -R
 
 chmod 2777 /minecraft/server/ -R
-
-chmod 2711 /minecraft/bin -R
-chmod 2711 /minecraft/restore -R
-chmod 2755 /minecraft/defaultplugins -R
 
 echo "chmod....done" >&1
