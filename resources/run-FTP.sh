d897f9b2281c47c5d6b598bd56afc4ee4dcf2215
haniokasai@Desktop <htek@haniokasai.com>
2019-12-07 22:11:40 +0900

    こんふぃぐftp

--- resources/run-FTP.sh
+++ resources/run-FTP.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Starting sftpd..." >&1
 sh /minecraft/resources/setPerm.sh
+sh /minecraft/resources/config-FTP.sh
 exec /usr/sbin/sshd -D -e &
 echo $! > /minecraft/bin/sftpd.pid
 cat /minecraft/bin/sftpd.pid
